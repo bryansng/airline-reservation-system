2e9b9464da4858b2b1da99ac20b5ce259cc0f6db
Braddy Yeoh <yeohbraddy@gmail.com>
2021-03-03 15:59:19 +0000

    Organised imports

--- backend/src/main/java/dreamwok/reservation/service/FlightService.java
+++ backend/src/main/java/dreamwok/reservation/service/FlightService.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dreamwok.reservation.model.Flight;
