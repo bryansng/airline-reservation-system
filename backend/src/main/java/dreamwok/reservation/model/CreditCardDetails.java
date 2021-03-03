d75d0aea05a4794bb55d044d19ce793134b32f84
Braddy Yeoh <yeohbraddy@gmail.com>
2021-03-03 15:57:14 +0000

    Used Spring relational mapping instead of string ID

--- backend/src/main/java/dreamwok/reservation/model/CreditCardDetails.java
+++ backend/src/main/java/dreamwok/reservation/model/CreditCardDetails.java
@@ -1,94 +1,110 @@
 package dreamwok.reservation.model;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 import dreamwok.reservation.core.creditcard.request.CreditCardRequest;
 
 @Entity
 @Table(name = "credit_card_details")
 public class CreditCardDetails {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @NotNull
-  private Long customerId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Customer customer;
 
-  private String nameOnCard;
-  private String cardNumber;
-  private String expiryDate;
-  private String securityCode;
+    private String nameOnCard;
+    private String cardNumber;
+    private String expiryDate;
+    private String securityCode;
 
-  public CreditCardDetails() {
-  }
+    public CreditCardDetails() {
+    }
 
-  public CreditCardDetails(Long customerId, String nameOnCard, String cardNumber, String expiryDate,
-      String securityCode) {
-    this.customerId = customerId;
-    this.nameOnCard = nameOnCard;
-    this.cardNumber = cardNumber;
-    this.expiryDate = expiryDate;
-    this.securityCode = securityCode;
-  }
+    public CreditCardDetails(Customer customer, String nameOnCard, String cardNumber, String expiryDate,
+            String securityCode) {
+        this.customer = customer;
+        this.nameOnCard = nameOnCard;
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.securityCode = securityCode;
+    }
 
-  public CreditCardDetails(Long customerId, CreditCardRequest creditCardRequest) {
-    this.setCustomerId(customerId);
-    this.updateCard(creditCardRequest);
-  }
+    public CreditCardDetails(Customer customer, CreditCardRequest creditCardRequest) {
+        this.setCustomer(customer);
+        this.updateCard(creditCardRequest);
+    }
 
-  public void updateCard(CreditCardRequest newCard) {
-    this.cardNumber = newCard.getCardNumber();
-    this.nameOnCard = newCard.getNameOnCard();
-    this.securityCode = newCard.getSecurityCode();
-    this.expiryDate = newCard.getExpiryDate();
-  }
+    public void updateCard(CreditCardRequest newCard) {
+        this.cardNumber = newCard.getCardNumber();
+        this.nameOnCard = newCard.getNameOnCard();
+        this.securityCode = newCard.getSecurityCode();
+        this.expiryDate = newCard.getExpiryDate();
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public Long getCustomerId() {
-    return customerId;
-  }
+    public Customer getCustomer() {
+        return customer;
+    }
 
-  public void setCustomerId(Long customerId) {
-    this.customerId = customerId;
-  }
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 
-  public String getNameOnCard() {
-    return nameOnCard;
-  }
+    public String getNameOnCard() {
+        return nameOnCard;
+    }
 
-  public void setNameOnCard(String nameOnCard) {
-    this.nameOnCard = nameOnCard;
-  }
+    public void setNameOnCard(String nameOnCard) {
+        this.nameOnCard = nameOnCard;
+    }
 
-  public String getCardNumber() {
-    return cardNumber;
-  }
+    public String getCardNumber() {
+        return cardNumber;
+    }
 
-  public void setCardNumber(String cardNumber) {
-    this.cardNumber = cardNumber;
-  }
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
 
-  public String getExpiryDate() {
-    return expiryDate;
-  }
+    public String getExpiryDate() {
+        return expiryDate;
+    }
 
-  public void setExpiryDate(String expiryDate) {
-    this.expiryDate = expiryDate;
-  }
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 
-  public String getSecurityCode() {
-    return securityCode;
-  }
+    public String getSecurityCode() {
+        return securityCode;
+    }
 
-  public void setSecurityCode(String securityCode) {
-    this.securityCode = securityCode;
-  }
+    public void setSecurityCode(String securityCode) {
+        this.securityCode = securityCode;
+    }
+
+    public String customerToString() {
+        return "derp: " + this.customer.getFirstName();
+    }
+
+    public String toString1() {
+        return "id: " + this.id + "\ncustomer: " + this.customer.toString() + "\nnameOnCard: " + this.nameOnCard
+                + "\ncardNumber: " + this.cardNumber + "\nexpiryDate: " + this.expiryDate + "\nsecurityCode: "
+                + this.securityCode;
+
+    }
+
+    public String toString2() {
+        return "id: " + this.id + "\nnameOnCard: " + this.nameOnCard + "\ncardNumber: " + this.cardNumber
+                + "\nexpiryDate: " + this.expiryDate + "\nsecurityCode: " + this.securityCode;
+
+    }
 }
--- backend/src/main/java/dreamwok/reservation/model/Customer.java
+++ backend/src/main/java/dreamwok/reservation/model/Customer.java
@@ -1,6 +1,7 @@
 package dreamwok.reservation.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -8,184 +9,196 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import dreamwok.reservation.core.creditcard.request.CreditCardRequest;
 import dreamwok.reservation.core.customer.request.CustomerRequest;
-import java.time.*;
-import java.util.List;
-
-import javax.persistence.*;
 
 @Entity
 @Table(name = "customers")
 public class Customer {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @NotEmpty
-  private String email;
-  private String firstName;
-  private String lastName;
-  private String address;
-  private String phoneNum;
-  private String roles = "USER";
+    @NotEmpty
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNum;
+    private String roles = "USER";
 
-  @Column(nullable = true)
-  private LocalDateTime bornOn;
-  private LocalDateTime joinedOn = LocalDateTime.now();
-  private String type = "member";
+    @Column(nullable = true)
+    private LocalDateTime bornOn;
+    private LocalDateTime joinedOn = LocalDateTime.now();
+    private String type = "member";
 
-  @OneToOne(cascade = CascadeType.ALL)
-  private Auth auth;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Auth auth;
 
-  @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
-  private List<Reservation> reservations;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Reservation> reservations;
 
-  @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
-  private List<Booking> bookings;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Booking> bookings;
 
-  public Customer() {
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<CreditCardDetails> creditCardDetails;
 
-  }
-
-  public Customer(String email, String firstName, String lastName, String address, String phoneNum, String type) {
-    this.setEmail(email);
-    this.setFirstName(firstName);
-    this.setLastName(lastName);
-    this.setAddress(address);
-    this.setPhoneNum(phoneNum);
-    this.setType(type);
-  }
-
-  public Customer(CustomerRequest customerRequest, String type) {
-    this.setEmail(customerRequest.getEmail());
-    this.setFirstName(customerRequest.getFirstName());
-    this.setLastName(customerRequest.getLastName());
-    this.setAddress(customerRequest.getAddress());
-    this.setPhoneNum(customerRequest.getPhoneNum());
-    this.setType(type);
-  }
-
-  public String getRoles() {
-    return roles;
-  }
-
-  public void setRoles(String roles) {
-    this.roles = roles;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  @JsonIgnore
-  public Auth getAuth() {
-    return auth;
-  }
-
-  public void setAuth(Auth auth) {
-    this.auth = auth;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPhoneNum() {
-    return phoneNum;
-  }
-
-  public void setPhoneNum(String phoneNum) {
-    this.phoneNum = phoneNum;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public LocalDateTime getJoinedOn() {
-    return joinedOn;
-  }
-
-  public void setJoinedOn(LocalDateTime joinedOn) {
-    this.joinedOn = joinedOn;
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void update(CustomerRequest customerRequest) {
-    this.setEmail(customerRequest.getEmail());
-    this.setFirstName(customerRequest.getFirstName());
-    this.setLastName(customerRequest.getLastName());
-    this.setAddress(customerRequest.getAddress());
-    this.setPhoneNum(customerRequest.getPhoneNum());
-    this.setType(type);
-    this.setAuth(this.auth);
-
-    switch (type.toLowerCase()) {
-      case "member":
-        this.setRoles("USER");
-        break;
-      case "librarian":
-        this.setRoles("ADMIN");
-        break;
+    public Customer() {
 
     }
-  }
 
-  public List<Reservation> getReservations() {
-    return reservations;
-  }
+    public Customer(String email, String firstName, String lastName, String address, String phoneNum, String type) {
+        this.setEmail(email);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAddress(address);
+        this.setPhoneNum(phoneNum);
+        this.setType(type);
+    }
 
-  public void setReservations(List<Reservation> reservations) {
-    this.reservations = reservations;
-  }
+    public Customer(CustomerRequest customerRequest, String type) {
+        this.setEmail(customerRequest.getEmail());
+        this.setFirstName(customerRequest.getFirstName());
+        this.setLastName(customerRequest.getLastName());
+        this.setAddress(customerRequest.getAddress());
+        this.setPhoneNum(customerRequest.getPhoneNum());
+        this.setType(type);
+    }
 
-  public List<Booking> getBookings() {
-    return bookings;
-  }
+    public String getRoles() {
+        return roles;
+    }
 
-  public void setBookings(List<Booking> bookings) {
-    this.bookings = bookings;
-  }
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonIgnore
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Auth auth) {
+        this.auth = auth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public LocalDateTime getJoinedOn() {
+        return joinedOn;
+    }
+
+    public void setJoinedOn(LocalDateTime joinedOn) {
+        this.joinedOn = joinedOn;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "creditCardDetails: " + this.creditCardDetails.get(0).toString2();
+    }
+
+    public void update(CustomerRequest customerRequest) {
+        this.setEmail(customerRequest.getEmail());
+        this.setFirstName(customerRequest.getFirstName());
+        this.setLastName(customerRequest.getLastName());
+        this.setAddress(customerRequest.getAddress());
+        this.setPhoneNum(customerRequest.getPhoneNum());
+        this.setType(type);
+        this.setAuth(this.auth);
+
+        switch (type.toLowerCase()) {
+            case "member":
+                this.setRoles("USER");
+                break;
+            case "librarian":
+                this.setRoles("ADMIN");
+                break;
+
+        }
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public List<CreditCardDetails> getCreditCardDetails() {
+        return creditCardDetails;
+    }
+
+    public void setCreditCardDetails(List<CreditCardDetails> creditCardDetails) {
+        this.creditCardDetails = creditCardDetails;
+    }
 }
