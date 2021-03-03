93685c64fe7119a5782d8c478d04dfddb5b07596
Braddy Yeoh <yeohbraddy@gmail.com>
2021-03-03 15:58:11 +0000

    Removed pre-populated cards

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -61,13 +61,6 @@ VALUES
   (31, 'AA 1249', 'LGA', 'MIA', '2021-04-01T06:00:00', '2021-04-01T09:04:00', 58, 4),
   (32, 'F9 2879', 'LGA', 'MIA', '2021-04-01T22:45:00', '2021-04-02T01:46:00', 40, 23);
 
-INSERT INTO credit_card_details
-  (customer_id, name_on_card, card_number, expiry_date, security_code)
-VALUES
-  (1, 'Braddy Yeoh', '4319 1234 1234 1234', '04/20', '123'),
-  (1, 'Braddy Yeoh', '4319 4321 4321 4321', '05/23', '321'),
-  (2, 'Cao Cao', '4319 5678 3453 2343', '01/24', '345'),
-  (3, 'Liu Bei', '5230 1322 4565 3455', '04/22', '456');
 -- --
 -- INSERT INTO books(id, book_name, isbn)
 -- VALUES (1, "Thinking Fast", "1234");
