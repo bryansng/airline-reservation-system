babbaa1996b14c90c6e78cfb8b51074c4f77d655
@lxemily <34338522+LxEmily@users.noreply.github.com>
2021-03-05 21:46:59 +0000

    fixed profile, reservation details is squished
    bc app.js has a width already

--- frontend/src/components/User/Profile.js
+++ frontend/src/components/User/Profile.js
@@ -6,62 +6,62 @@ import { IconContext } from "react-icons";
 import { Link } from "react-router-dom";
 
 const Container = styled.div.attrs({
-  className: `flex flex-column pr6 pl6`
-})``
+  className: `flex flex-column`,
+})``;
 
 const TitleContainer = styled.div.attrs({
-  className: `pa3 mb5`
-})``
+  className: `pa3 mb5`,
+})``;
 
 const Title = styled.p.attrs({
-  className: `f1 measure lh-title fw1 mt3 ml6`
-})``
+  className: `f1 measure lh-title fw1 mt3 mh0 tc w-100`,
+})``;
 
 const IconContainer = styled.div.attrs({
-  className: `pa3 flex`
-})``
+  className: `flex justify-around`,
+})``;
 
 const Icon = styled.div.attrs({
-  className: `ma4`
-})``
+  className: `mv4 mh0`,
+})``;
 
 const IconTitleDiv = styled.div.attrs({
-  className: `mt3`
-})``
+  className: `mt3`,
+})``;
 
 const IconTitle = styled.p.attrs({
-  className: `f4`
-})``
+  className: `f4`,
+})``;
 
 // https://reactrouter.com/web/api/match
 const Profile = ({ match }) => {
   // get user id from match.params.id and GET user data.
-  const testUserId = { 
+  const testUserId = {
     id: "1",
     firstName: "Braddy",
     lastName: "Yeoh",
     phone: "123",
     address: "123 road lane",
-    email: "braddy.yeoh@ucdconnect.ie"
+    email: "braddy.yeoh@ucdconnect.ie",
   };
 
   const icons = [
     {
-      icon:  <FaUser/>,
+      icon: <FaUser />,
       title: "Personal Details",
-      suburl: "personaldetails"
+      suburl: "personaldetails",
     },
     {
-      icon: <FaPlane/>,
+      icon: <FaPlane />,
       title: "Reservations",
-      suburl: "reservations"
+      suburl: "reservations",
     },
     {
-      icon: <FaCreditCard/>,
+      icon: <FaCreditCard />,
       title: "Card Details",
-      suburl: "creditcards"
-    }
-  ]
+      suburl: "creditcards",
+    },
+  ];
 
   return (
     <Container>
@@ -69,31 +69,26 @@ const Profile = ({ match }) => {
         <Title>Welcome, {testUserId.firstName}</Title>
       </TitleContainer>
       <IconContainer>
-        {
-          icons.map((icon, key) => {
-            return (
-              <Link
-                key={key}
-                style={{ color: 'dimgray' }} 
-                className="ba b--silver br4 w-33 tc ma4 grow pointer dim" 
-                to={`/user/profile/` + match.params.id + "/" + icon.suburl}>
-                  <IconContext.Provider value={{size: "15em"}}>
-                    <Icon>
-                      {icon.icon}
-                    </Icon>
-                    <IconTitleDiv>
-                      <IconTitle>
-                      {icon.title}
-                      </IconTitle>
-                    </IconTitleDiv>
-                  </IconContext.Provider>
-              </Link>
-            )
-          })
-        }
+        {icons.map((icon, key) => {
+          return (
+            <Link
+              key={key}
+              style={{ color: "dimgray" }}
+              className="ba b--silver br4 tc mh2 pa3 grow pointer dim"
+              to={`/user/profile/` + match.params.id + "/" + icon.suburl}
+            >
+              {/* value={{ size: "15em" }} */}
+              <IconContext.Provider value={{ size: "90%" }}>
+                <Icon>{icon.icon}</Icon>
+                <IconTitleDiv>
+                  <IconTitle>{icon.title}</IconTitle>
+                </IconTitleDiv>
+              </IconContext.Provider>
+            </Link>
+          );
+        })}
       </IconContainer>
     </Container>
-    
   );
 };
 
--- frontend/src/components/User/Reservations/ReservationDetails.js
+++ frontend/src/components/User/Reservations/ReservationDetails.js
@@ -1,96 +1,193 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import * as dayjs from "dayjs";
+// dayjs.extend(isBetween);
 
 import rest_endpoints from "../../../config/rest_endpoints.json";
 
 const Container = styled.div.attrs({
-    className: `flex flex-column pr6 pl6 justify-content-center`
-})``
+  className: `flex flex-column pr6 pl6 justify-content-center`,
+})``;
 
 const HeaderRow = styled.div.attrs({
-    className: `flex`
-})``
+  className: `flex`,
+})``;
 
 const TitleContainer = styled.div.attrs({
-    className: `pa3 mb2 w-50`
-})``
-  
+  className: `pa3 mb2 w-50`,
+})``;
+
 const Title = styled.p.attrs({
-    className: `f2 measure fw1 mt3 ml-5`
-})``
+  className: `f2 measure fw1 mt3 ml-5`,
+})``;
 
 const ReservationContainer = styled.div.attrs({
-  className: `flex flex-column`
-})``
+  className: `flex flex-column`,
+})``;
 
 const ReservationDiv = styled.div.attrs({
-  className: `outline flex pa3 ma3`
-})``
+  className: `outline flex pa3 ma3`,
+})``;
 
 const ReservationText = styled.p.attrs({
-  className: `f3 fw3`
-})``
+  className: `f3 fw3`,
+})``;
 
 const NoReservationContainer = styled.div.attrs({
-  className: ``
-})``
+  className: ``,
+})``;
 
 const NoReservation = styled.p.attrs({
-  className: `tc f2 fw3`
-})``
+  className: `tc f2 fw3`,
+})``;
 
 const ReservationDetailDiv = styled.div.attrs({
-  className: `w-25`
-})``
+  className: `w-25`,
+})``;
 
 const View = styled.p.attrs({
-  className: `f3 dim pointer fw3`
-})``
-
+  className: `f3 dim pointer fw3`,
+})``;
 
 const ReservationDetails = ({ match }) => {
+  const [reservations, setReservations] = useState([]);
 
-    const [reservations, setReservations] = useState([])
-  
-    const url = "http://localhost:8080/reservations/9"
+  const url = "http://localhost:8080/reservations/9";
 
-    useEffect(() => {
-      fetch(url)
-        .then((resp) => {
-          if (resp.ok) {
-            return resp.json();
-          }
-          throw new Error(`${resp.status} Error retrieving customer.`);
-        })
-        .then((res) => {
-            console.log(res)
-            const reservations = res.reservations
-            setReservations(reservations)
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }, []);
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        throw new Error(`${resp.status} Error retrieving customer.`);
+      })
+      .then((res) => {
+        console.log(res);
+        const reservations = res.reservations;
+        setReservations(reservations);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    return (
-      <Container>
-         <HeaderRow>
-            <TitleContainer>
-                <Title>Reservations</Title>
-            </TitleContainer>
+  return (
+    <>
+      <h2>Show Reservation</h2>
+      <Accordion defaultActiveKey="0">
+        {reservations.length === 0 ? (
+          <Card>
+            <Card.Body>No reservations</Card.Body>
+          </Card>
+        ) : (
+          reservations.map((reservation, key) => {
+            return (
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    variant="link"
+                    eventKey="0"
+                  >
+                    {`${reservation.flight.departureAirport} to ${reservation.flight.arrivalAirport}`}{" "}
+                    on{" "}
+                    {`${dayjs(reservation.flight.departureDateTime).format(
+                      "DD/MM/YYYY"
+                    )}`}
+                    &#8212; {reservation.flight.flightName}
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey={key}>
+                  <Card.Body>
+                    <h5 className="mb-3">
+                      {`${reservation.flight.departureAirport} to ${reservation.flight.arrivalAirport}`}{" "}
+                      &#8212; {reservation.flight.flightName}
+                    </h5>
+                    <Card.Subtitle className="mb-4 text-muted">
+                      {`${dayjs(reservation.flight.departureDateTime).format(
+                        "DD/MM/YYYY"
+                      )}`}
+                      {", "}
+                      {`${dayjs(reservation.flight.departureDateTime).format(
+                        "HH:mm"
+                      )}`}{" "}
+                      &#8212;{" "}
+                      {`${dayjs(reservation.flight.arrivalDateTime).format(
+                        "DD/MM/YYYY"
+                      )}`}
+                      {", "}
+                      {`${dayjs(reservation.flight.arrivalDateTime).format(
+                        "HH:mm"
+                      )}`}
+                    </Card.Subtitle>
+
+                    <div className="mv2">
+                      <div className="gray f5">Booking number</div>
+                      <div className="lh-copy">{reservation.id}</div>
+                    </div>
+                    <div className="mv2">
+                      <div className="gray f5">Booking name</div>
+                      <div className="lh-copy">{`${reservation.customer.lastName}`}</div>
+                    </div>
+                    <div className="mv2">
+                      <div className="gray f5">Number of tickets</div>
+                      <div className="lh-copy">
+                        {reservation.bookings.length}
+                      </div>
+                    </div>
+                    <div className="mv2">
+                      <div className="gray f5">Flight number</div>
+                      <div className="lh-copy">
+                        {reservation.flight.flightName}
+                      </div>
+                    </div>
+                    <div className="mv2">
+                      <div className="gray f5">Check in before</div>
+                      <div className="lh-copy">
+                        {dayjs(reservation.flight.departureDateTime)
+                          .subtract(1, "hour")
+                          .format("HH:mm")}
+                      </div>
+                    </div>
+                    {reservation.bookings.map(
+                      (currPassengerBooking, currIndex) => (
+                        <div className="mv2">
+                          <div className="gray f5">
+                            Passenger {currIndex + 1}
+                          </div>
+                          <div className="lh-copy">{`${currPassengerBooking.firstName} ${currPassengerBooking.lastName}`}</div>
+                        </div>
+                      )
+                    )}
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            );
+          })
+        )}
+      </Accordion>
+      {/* <Container>
+        <HeaderRow>
+          <TitleContainer>
+            <Title>Reservations</Title>
+          </TitleContainer>
         </HeaderRow>
         <ReservationContainer>
-          {
-            reservations.length === 0 ? 
+          {reservations.length === 0 ? (
             <NoReservationContainer>
               <NoReservation>No Reservations</NoReservation>
-            </NoReservationContainer> :
+            </NoReservationContainer>
+          ) : (
             reservations.map((reservation, key) => {
               return (
                 <ReservationDiv key={key}>
                   <ReservationDetailDiv>
                     <ReservationText>
-                      <b>Departure Time - </b>{reservation.flight.departureTime}
+                      <b>Departure Time - </b>
+                      {reservation.flight.departureTime}
                     </ReservationText>
                   </ReservationDetailDiv>
                   <ReservationDetailDiv>
@@ -100,7 +197,8 @@ const ReservationDetails = ({ match }) => {
                   </ReservationDetailDiv>
                   <ReservationDetailDiv>
                     <ReservationText>
-                      <b>Departure Airport - </b>{reservation.flight.departureAirport}
+                      <b>Departure Airport - </b>
+                      {reservation.flight.departureAirport}
                     </ReservationText>
                   </ReservationDetailDiv>
                   <ReservationDetailDiv className="tr">
@@ -108,23 +206,22 @@ const ReservationDetails = ({ match }) => {
                       {reservation.flight.flightName}
                     </ReservationText>
                   </ReservationDetailDiv>
-                  <ReservationDetailDiv className ="tr">
+                  <ReservationDetailDiv className="tr">
                     <ReservationText>
                       {reservation.reservationStatus}
                     </ReservationText>
                   </ReservationDetailDiv>
                   <ReservationDetailDiv className="tr">
-                    <View>
-                      View
-                    </View>
+                    <View>View</View>
                   </ReservationDetailDiv>
-              </ReservationDiv>
-              )
+                </ReservationDiv>
+              );
             })
-          }
+          )}
         </ReservationContainer>
-      </Container>
-    );
-  };
-  
-  export default ReservationDetails;
+      </Container> */}
+    </>
+  );
+};
+
+export default ReservationDetails;
