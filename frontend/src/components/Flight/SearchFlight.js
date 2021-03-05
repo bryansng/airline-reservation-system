702c311139e926a7ee64d2b201ab33ecdd3d602a
@lxemily <34338522+LxEmily@users.noreply.github.com>
2021-03-05 20:55:58 +0000

    Flight results

--- frontend/src/components/Flight/SearchFlight.js
+++ frontend/src/components/Flight/SearchFlight.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 import * as dayjs from "dayjs";
 import { Link } from "react-router-dom";
 import { rest_endpoints } from "../../config/rest_endpoints.json";
 const { flight: flight_apis } = rest_endpoints;
 
 const Button = styled.button.attrs({
-  className: `ma2 relative w-100 b--gray ma0 br2 ba hover-bg-light-gray tc`,
+  className: `relative w-100 b--gray mh0 mb2 br2 ba hover-bg-light-gray tc`,
 })`
   padding: 6px 20px;
   transition: 0.15s ease-out;
@@ -24,6 +25,11 @@ const Button = styled.button.attrs({
 const Grid = styled.div.attrs({})`
   display: grid;
   grid-template-columns: 1fr 1fr;
+`;
+
+const ResultGrid = styled.div.attrs({})`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
 `;
 
 const SearchFlight = () => {
@@ -134,31 +140,16 @@ const SearchFlight = () => {
           </div>
         </div>
       </Form>
-      <div className="mv2">
-        {flights &&
-          flights.map((currFlight, currIndex) => {
-            const flightDuration = dayjs(currFlight.arrivalDateTime).diff(
-              dayjs(currFlight.departureDateTime)
-            );
+      <Card className="mv2">
+        <Card.Header>Select a flight</Card.Header>
+        <ListGroup variant="flush">
+          {flights &&
+            flights.map((currFlight, currIndex) => {
+              const flightDuration = dayjs(currFlight.arrivalDateTime).diff(
+                dayjs(currFlight.departureDateTime)
+              );
 
-            return (
-              <div key={currIndex} className="mb3">
-                <div>{`${dayjs(currFlight.departureDateTime).format(
-                  "HH:mm"
-                )} - ${dayjs(currFlight.arrivalDateTime).format(
-                  "HH:mm"
-                )}`}</div>
-
-                <div>
-                  {dayjs(currFlight.departureDateTime).format("ddd, MMM DD")}
-                </div>
-
-                <div>{`${currFlight.departureAirport} - ${currFlight.arrivalAirport}`}</div>
-
-                <div>{`Duration: ${dayjs(flightDuration).format(
-                  "HH:mm"
-                )}`}</div>
-
+              return (
                 <Link
                   to={{
                     pathname: `/book/${currFlight.id}`,
@@ -167,17 +158,114 @@ const SearchFlight = () => {
                       numPassengers: numberOfPassengers,
                     },
                   }}
+                  style={{ textDecoration: "none", color: "#212529" }}
                 >
-                  <Button type="button">Select Flight</Button>
-                </Link>
+                  <ListGroup.Item
+                    key={currIndex}
+                    className="hover-bg-light-gray"
+                    style={{
+                      transition: "all 0.15s ease-out",
+                    }}
+                  >
+                    <div className="flex justify-between flex-wrap">
+                      <div>
+                        <h5 className="mb-3">
+                          {`${currFlight.departureAirport} to ${currFlight.arrivalAirport}`}{" "}
+                          &#8212; {currFlight.flightName}
+                        </h5>
 
-                <div>{`EUR ${
-                  currFlight.flightPrice * numberOfPassengers
-                } (EUR ${currFlight.flightPrice}*${numberOfPassengers})`}</div>
-              </div>
-            );
-          })}
-      </div>
+                        <Card.Subtitle className="mb-4 text-muted">
+                          {`${dayjs(currFlight.departureDateTime).format(
+                            "DD/MM/YYYY"
+                          )}`}
+                          {", "}
+                          {`${dayjs(currFlight.departureDateTime).format(
+                            "HH:mm"
+                          )}`}{" "}
+                          &#8212;{" "}
+                          {`${dayjs(currFlight.arrivalDateTime).format(
+                            "DD/MM/YYYY"
+                          )}`}
+                          {", "}
+                          {`${dayjs(currFlight.arrivalDateTime).format(
+                            "HH:mm"
+                          )}`}
+                        </Card.Subtitle>
+                      </div>
+                      {/* <div>
+                        <div className="mv2">
+                          <div className="dark-gray">Duration</div>
+                          <div className="lh-copy">
+                            {dayjs(flightDuration).format("HH:mm")}
+                          </div>
+                        </div>
+                      </div> */}
+                    </div>
+
+                    <ResultGrid>
+                      <div className="mv2">
+                        <div className="dark-gray">Duration</div>
+                        <div className="lh-copy">
+                          {dayjs(flightDuration).format("HH:mm")}
+                        </div>
+                      </div>
+                      <div className="mv2">
+                        <div className="dark-gray">Price per ticket</div>
+                        <div className="lh-copy">{currFlight.flightPrice}</div>
+                      </div>
+                      <div className="mv2">
+                        <div className="dark-gray">Number of tickets</div>
+                        <div className="lh-copy">{numberOfPassengers}</div>
+                      </div>
+
+                      <div className="mv2">
+                        <div className="dark-gray">Total Price</div>
+                        <div className="lh-copy">
+                          &#8364; {currFlight.flightPrice * numberOfPassengers}
+                        </div>
+                      </div>
+                      {/* <div>
+                        <Link
+                          to={{
+                            pathname: `/book/${currFlight.id}`,
+                            state: {
+                              flightId: currFlight.id,
+                              numPassengers: numberOfPassengers,
+                            },
+                          }}
+                        >
+                          <Button type="button">Select Flight</Button>
+                        </Link>
+                      </div> */}
+                    </ResultGrid>
+
+                    {/* <div>{`${dayjs(currFlight.departureDateTime).format(
+                    "HH:mm"
+                  )} - ${dayjs(currFlight.arrivalDateTime).format(
+                    "HH:mm"
+                  )}`}</div>
+
+                  <div>
+                    {dayjs(currFlight.departureDateTime).format("ddd, MMM DD")}
+                  </div>
+
+                  <div>{`${currFlight.departureAirport} - ${currFlight.arrivalAirport}`}</div>
+
+                  <div>{`Duration: ${dayjs(flightDuration).format(
+                    "HH:mm"
+                  )}`}</div> */}
+
+                    {/* <div>{`EUR ${
+                    currFlight.flightPrice * numberOfPassengers
+                  } (EUR ${
+                    currFlight.flightPrice
+                  }*${numberOfPassengers})`}</div> */}
+                  </ListGroup.Item>
+                </Link>
+              );
+            })}
+        </ListGroup>
+      </Card>
     </>
   );
 };
